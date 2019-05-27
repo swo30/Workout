9bcfd930c277cc33638a8b11b54805579e39a610
swo30 <swo30@outlook.com>
2019-05-27 13:00:51 -0700

    can transition to groups

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.workout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private Button newWorkoutButton;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,11 +23,9 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
+
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_back);
                     return true;
             }
             return false;
@@ -37,6 +39,19 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        newWorkoutButton = (Button) findViewById(R.id.newWorkoutButton);
+        newWorkoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGroups();
+            }
+        });
+    }
+
+    public void openGroups(){
+        Intent intent = new Intent(this, groups.class);
+        startActivity(intent);
     }
 
 }
