73d01c26eb41652642623501899c5903efea5159
swo30 <swo30@outlook.com>
2019-05-27 13:05:06 -0700

    removed botton navigation

--- app/src/main/java/com/example/workout/MainActivity.java
+++ app/src/main/java/com/example/workout/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.workout;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.NonNull;
 import android.view.MenuItem;
@@ -14,31 +13,14 @@ public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
     private Button newWorkoutButton;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_back);
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
 
         newWorkoutButton = (Button) findViewById(R.id.newWorkoutButton);
         newWorkoutButton.setOnClickListener(new View.OnClickListener() {
