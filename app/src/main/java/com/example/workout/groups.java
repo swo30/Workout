50326e6a8525edeb4d54c7a0c586edf700b498f6
swo30 <swo30@outlook.com>
2019-05-30 13:02:16 -0700

    layout fixed on groups

--- app/src/main/java/com/example/workout/groups.java
+++ app/src/main/java/com/example/workout/groups.java
@@ -8,6 +8,6 @@ public class groups extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_groups);
+        setContentView(R.layout.groups);
     }
 }
