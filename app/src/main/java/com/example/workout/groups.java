686bd99020d546efb10b3f78f94408774de94d23
swo30 <swo30@outlook.com>
2019-05-31 14:04:58 -0700

    put the logic behind the checboxes

--- app/src/main/java/com/example/workout/groups.java
+++ app/src/main/java/com/example/workout/groups.java
@@ -3,8 +3,10 @@ package com.example.workout;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.CheckBox;
 
 public class groups extends AppCompatActivity {
@@ -17,6 +19,9 @@ public class groups extends AppCompatActivity {
     private CheckBox shoulderCheckBox ;
     private CheckBox legCheckBox ;
 
+    private boolean oneCheckBox = false;
+    public int checkboxMuscleGroups = 0;
+
 
 
 
@@ -28,12 +33,12 @@ public class groups extends AppCompatActivity {
 
         nextButton = (Button) findViewById(R.id.nextButton);
 
-        chestCheckBox = (CheckBox)findViewById(R.id.chestCheckBox);
-        backCheckBox = (CheckBox)findViewById(R.id.backCheckBox);
-        biCheckBox = (CheckBox)findViewById(R.id.biCheckBox);
-        triCheckBox = (CheckBox)findViewById(R.id.triCheckBox);
-        shoulderCheckBox = (CheckBox)findViewById(R.id.shoulderCheckBox);
-        legCheckBox = (CheckBox)findViewById(R.id.legCheckBox);
+        chestCheckBox = findViewById(R.id.chestCheckBox);
+        backCheckBox = findViewById(R.id.backCheckBox);
+        biCheckBox = findViewById(R.id.biCheckBox);
+        triCheckBox = findViewById(R.id.triCheckBox);
+        shoulderCheckBox = findViewById(R.id.shoulderCheckBox);
+        legCheckBox = findViewById(R.id.legCheckBox);
 
 
 
@@ -45,13 +50,89 @@ public class groups extends AppCompatActivity {
         });
 
 
+        chestCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(((CompoundButton) view).isChecked()){
+                    oneCheckBox = true;
+                    checkboxMuscleGroups = checkboxMuscleGroups|32; //32 is 10 0000
+                }else{
+                    oneCheckBox = false;
+                    checkboxMuscleGroups = checkboxMuscleGroups^32; //32 is 10 0000
 
+                }
+            }
+        });
 
+        backCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(((CompoundButton) view).isChecked()){
+                    oneCheckBox = true;
+                    checkboxMuscleGroups = checkboxMuscleGroups|16; //16 is 01 0000
+                }else{
+                    oneCheckBox = false;
+                    checkboxMuscleGroups = checkboxMuscleGroups^16; //16 is 01 0000
 
+                }
+            }
+        });
+        biCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(((CompoundButton) view).isChecked()){
+                    oneCheckBox = true;
+                    checkboxMuscleGroups = checkboxMuscleGroups|8; //8 is 00 1000
+                }else{
+                    oneCheckBox = false;
+                    checkboxMuscleGroups = checkboxMuscleGroups^8; //8 is 00 1000
 
+                }
+            }
+        });
+        triCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(((CompoundButton) view).isChecked()){
+                    oneCheckBox = true;
+                    checkboxMuscleGroups = checkboxMuscleGroups|4; //4 is 00 0100
+                }else{
+                    oneCheckBox = false;
+                    checkboxMuscleGroups = checkboxMuscleGroups^4; //4 is 00 0100
 
+                }
+            }
+        });
+        shoulderCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(((CompoundButton) view).isChecked()){
+                    oneCheckBox = true;
+                    checkboxMuscleGroups = checkboxMuscleGroups|2; //2 is 00 0010
+                }else{
+                    oneCheckBox = false;
+                    checkboxMuscleGroups = checkboxMuscleGroups^2; //2 is 00 0010
 
+                }
+            }
+        });
+        legCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(((CompoundButton) view).isChecked()){
+                    oneCheckBox = true;
+                    checkboxMuscleGroups = checkboxMuscleGroups|1; //1 is 00 0001
+                }else{
+                    oneCheckBox = false;
+                    checkboxMuscleGroups = checkboxMuscleGroups^1; //1 is 00 0001
+                }
+            }
+        });
 
+        System.out.print(oneCheckBox);
+        if (oneCheckBox) {
+            nextButton.isEnabled();
+        }
 
 
     }
